6bd2b5ba3d3b3d28abfde36d53d7a232483b41c2
Philip Le <lephil505006@gmail.com>
2023-11-16 09:48:42 -0600

    implenting downloads and uploads

--- js/app.js
+++ js/app.js
@@ -1,26 +1,61 @@
-const express = require('express');
-const multer = require('multer');
-const fs = require('fs');
-const path = require('path');
+// Requiring express to handle routing 
+const express = require("express");
 
+// The fileUpload npm package for handling 
+// file upload functionality 
+const fileUpload = require("express-fileupload");
+
+// Creating app 
 const app = express();
 
-// Serve static files from the 'public' folder
-app.use(express.static('public'));
+// Passing fileUpload as a middleware 
+app.use(fileUpload());
 
-// Multer setup for file uploads
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, 'uploads/')
-    },
-    filename: function (req, file, cb) {
-        cb(null, file.originalname)
-    }
+// For handling the upload request 
+app.post("/upload", function (req, res) {
+
+    // When a file has been uploaded 
+    if (req.files && Object.keys(req.files).length !== 0) {
+
+        // Uploaded path 
+        const uploadedFile = req.files.uploadFile;
+
+        // Logging uploading file 
+        console.log(uploadedFile);
+
+        // Upload path 
+        const uploadPath = __dirname
+            + "/uploads/" + uploadedFile.name;
+
+        // To save the file using mv() function 
+        uploadedFile.mv(uploadPath, function (err) {
+            if (err) {
+                console.log(err);
+                res.send("Failed !!");
+            } else res.send("Successfully Uploaded !!");
+        });
+    } else res.send("No file uploaded !!");
 });
 
-const upload = multer({ storage: storage });
+// To handle the download file request 
+app.get("/download", function (req, res) {
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    // The res.download() talking file path to be downloaded 
+    res.download(__dirname + "/download_gfg.txt", function (err) {
+        if (err) {
+            console.log(err);
+        }
+    });
+});
+
+// GET request to the root of the app 
+app.get("/", function (req, res) {
+
+    // Sending index.html file as response to the client 
+    res.sendFile(__dirname + "/index.html");
+});
+
+// Makes app listen to port 3000 
+app.listen(3000, function (req, res) {
+    console.log("Started listening to port 3000");
 });
